cd8ff028a0b243d3f5efe303013ba35ceab0f35f
Steven Zuluaga <steven000069@hotmail.com>
2022-01-20 19:30:05 -0500

    Setup Maps

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import tw from "tailwind-react-native-classnames"
+import Map from '../components/Map';
 
 const MapScreen = () => {
     return (
         <View>
-            <Text>Here is the map</Text>
+           
+            <View style={tw`h-1/2`}>
+                <Map/>
+            </View>
+            <View style={tw`h-1/2`}></View>
         </View>
-    )
+    );
 }
 
 export default MapScreen
